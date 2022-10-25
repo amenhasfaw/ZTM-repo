ccde478651207cd23aaac3bb0351a8fc7a3ea08b
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2022-10-25 16:50:34 +0530

    Update:

--- Playground/script.js
+++ Playground/script.js
@@ -1,5 +1,21 @@
+// Random code I used for practising different concepts
+// - basic Javascript
+// - DOM Manipulation
+// - Advanced Javascript
+
+///////////////////////////////////////////////////////////
+
+// Forked a previous mini-project I did to Play with
+
+///////////////////////////////////////////////////////////
+
+
 const container = document.getElementById('container');
 const img = document.querySelector('img');
+
+
+// const amen = prompt("Name:")
+//console.log(amen)
 
 container.addEventListener("mousemove", (e) => {
     const x = e.clientX - e.target.offsetLeft;
