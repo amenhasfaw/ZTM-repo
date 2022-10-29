4b3fbb4ec2a12187eeb817f30e1b473722ca8271
amenhasfaw <105349999+amenhasfaw@users.noreply.github.com>
2022-10-29 16:05:24 +0530

    Class Component Structure

---
+++ reactClass.js
@@ -0,0 +1,22 @@
+// // A react class component structure
+
+
+// class App extends React.Component {
+//     constructor(props){
+//         super(props)
+//         this.state
+//     }    
+//       
+//     methods()
+//     
+// }
+
+// render (){
+//     return(
+//         <div>
+//             //JSX to render
+//         </div>
+//        )
+// }
+
+// ReactDOM.render(<App/>, document.getElementById('root'));
